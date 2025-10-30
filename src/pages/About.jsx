3847c74df0c15e8277cf9ab2be3a22f5eae08f16
Aziz Ahmad <homan6949@gmail.com>
2025-10-30 14:30:04 +0430

    1

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
-function About(){
-    return(
-        <section className="about">
-            <h2>About Me</h2>
-            <p>
-                I'm a professional web developer with 5 years of experience,
-                creating modern and responsive websites, looking for new opportunities.
-            </p>
-        </section>
-    )
+function About() {
+  return (
+    <section className="about">
+      <h2>About Me</h2>
+      <p>
+        My Name is Aziz Ahmad Hadiari and I am a Full-Stack Web Developer. I
+        have a passion for creating dynamic and responsive web applications.
+        With a strong foundation in both front-end and back-end technologies, I
+        enjoy bringing ideas to life through code.
+      </p>
+    </section>
+  );
 }
 
 export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
   return (
     <header className="header">
-      <h1>Ahmad Khan</h1>
+      <h1>Aziz Ahmad</h1>
       <p>Software Engineer | Web Developer</p>
       <a href="/resume.pdf" className="btn">Download Resume</a>
     </header>

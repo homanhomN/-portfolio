741a68a5dfbaf9399a8b903686e86e076cf38be4
Aziz Ahmad <homan6949@gmail.com>
2025-11-02 14:12:36 +0430

    1

--- src/pages/ProjectCard.jsx
+++ src/pages/ProjectCard.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-function ProjectCard({title, description, img, link}){
+function ProjectCard({title, description, link}){
     return(
         <div className="project-card">
-            <img src={img} alt={title}/>
+
             <h3>{title}</h3>
             <p>{description}</p>
             <a href={link} className="btn">View Project</a>
